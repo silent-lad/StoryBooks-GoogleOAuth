23046a100ab07928c8b185204265c1212b0fb70b
DIVYANSH TRIPATHI <30868400+silentlad1@users.noreply.github.com>
2018-04-21 16:53:29 +0530

    google aut

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://divyansh:divyansh@ds247479.mlab.com:47479/storybooks-silentlad-dev',
+  googleClientID:'1052720785971-o8pofu4428dem2rihmehnvt91pjfbfbr.apps.googleusercontent.com',
+  googleClientSecret:'P2ro__UfTu81WHvvRuxxr30I'
+}
