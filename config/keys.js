b258d3d2c768ca87d53ccbfc26898c806c62b5ab
DIVYANSH TRIPATHI <30868400+silentlad1@users.noreply.github.com>
2018-04-23 17:26:12 +0530

    FINAL@1.0.0

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:'mongodb://divyansh:divyansh@ds247479.mlab.com:47479/storybooks-silentlad-dev',
-  googleClientID:'1052720785971-o8pofu4428dem2rihmehnvt91pjfbfbr.apps.googleusercontent.com',
-  googleClientSecret:'P2ro__UfTu81WHvvRuxxr30I'
+if(process.env.NODE_ENV=='production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev')
 }
